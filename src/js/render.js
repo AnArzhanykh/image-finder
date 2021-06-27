9ef5d48abb37da20c59c5ba4e368d28e2b56f397
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-06-27 17:34:44 +0300

    observ

--- src/js/render.js
+++ src/js/render.js
@@ -13,8 +13,9 @@ function renderListCards(arr){
     isBtnVisible(arr);
     const markup = listImages(arr);
     ref.galleryRef.insertAdjacentHTML('beforeend', markup);
-    observ()
-
+    if(arr.length >= 12){
+      observ()
+    }
 }
 
 function observ() {
@@ -22,20 +23,26 @@ function observ() {
     rootMargin: '0px',
     threshold: 1.0
   }
-
+  
   function callback(entries, observer) { 
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         console.log('entry', entry);
-        render()
+
+        
+          render()
+        
+        
       }
     });
   };
-
   const elementToObserve = document.querySelector('.list-img:last-child');
+  
+  
   const observer = new IntersectionObserver(callback, options);
-
   observer.observe(elementToObserve);
+  
+  
 }
  
 
